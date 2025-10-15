e00e9414e96b1fab90da231ebce847f50986601d
MikaSantos01 <micaias08deus@gmail.com>
2025-10-15 20:37:49 -0300

    atividade_03

---
+++ atividade_03/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Factory as FakerFactory;
+
+class DatabaseSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    public function run()
+    {
+        FakerFactory::create()->unique(true);
+        $this->call([
+            CategorySeeder::class,
+            AuthorPublisherBookSeeder::class,
+        ]);
+    }
+
+}
